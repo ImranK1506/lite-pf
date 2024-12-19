38611182cdb9a794e22d63e74edc30157aaf8d35
Imran Khan <m_imran_khan@me.com>
2024-12-19 14:05:12 +0100

    add tailwind typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: { extend: {}
-	},
-	plugins: [require("daisyui")],
+	theme: { extend: {} },
+	plugins: [require('@tailwindcss/typography') ,require('daisyui')],
 	daisyui: {
-		// themes:["light", "dark", "forest"],
-		themes: true,
+		themes: ['winter', 'night'],
+		darkMode:['selector', '[data-theme="night"]'],
+		// Example of adding a custom theme
+		// themes: [
+		// 	{
+		// 		mytheme: {
+		// 			primary: "#f3c614",
+		// 			"primary-focus": "#f7ca4d",
+		// 			"primary-content": "#000",
+		//
+		// 			secondary: "#f000b8",
+		// 			"secondary-focus": "#bd0091",
+		// 			"secondary-content": "#fff",
+		//
+		// 			accent: "#37cdbe",
+		// 			"accent-focus": "#2aa79b",
+		// 			"accent-content": "#fff",
+		//
+		// 			neutral: "#3d4451",
+		// 			"neutral-focus": "#2a2e37",
+		// 			"neutral-content": "#ffffff",
+		// 		},
+		// 	}
+		// ]
 	},
 }
