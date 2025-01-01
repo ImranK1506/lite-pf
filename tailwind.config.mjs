5c0450b562ae4308d679b332e39f25a3393893de
Imran Khan <m_imran_khan@me.com>
2025-01-01 19:39:30 +0100

    work on grid

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,28 +5,6 @@ export default {
 	plugins: [require('@tailwindcss/typography') ,require('daisyui')],
 	daisyui: {
 		themes: ['winter', 'night'],
-		darkMode:['selector', '[data-theme="night"]'],
-		// Example of adding a custom theme
-		// themes: [
-		// 	{
-		// 		mytheme: {
-		// 			primary: "#f3c614",
-		// 			"primary-focus": "#f7ca4d",
-		// 			"primary-content": "#000",
-		//
-		// 			secondary: "#f000b8",
-		// 			"secondary-focus": "#bd0091",
-		// 			"secondary-content": "#fff",
-		//
-		// 			accent: "#37cdbe",
-		// 			"accent-focus": "#2aa79b",
-		// 			"accent-content": "#fff",
-		//
-		// 			neutral: "#3d4451",
-		// 			"neutral-focus": "#2a2e37",
-		// 			"neutral-content": "#ffffff",
-		// 		},
-		// 	}
-		// ]
-	},
+		},
+	darkMode:['selector', '[data-theme="night"]'],
 }
