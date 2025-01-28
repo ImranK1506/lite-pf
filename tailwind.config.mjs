7429218a584e728c3d3837b54ac513cf77cad233
Imran Khan <m_imran_khan@me.com>
2025-01-28 12:50:14 +0100

    working on font style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: { },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+				heading: ['Lexend', 'sans-serif'],
+			},
+		},
+	},
 	plugins: [require('@tailwindcss/typography') ,require('daisyui')],
 	daisyui: {
 		themes: ['winter', 'night'],
