1db2d81480c4aa083160eabb109d716ccce4d174
Imran Khan <m_imran_khan@me.com>
2025-03-07 20:13:24 +0100

    wip: rollback fixed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	// theme: {
-	// 	extend: {
-	// 		fontFamily: {
-	// 			heading: ['"DM Sans"', 'sans-serif'],
-	// 			body: ['"Fira Sans"', 'sans-serif'],
-	// 		},
-	// 	},
-	// },
-	// plugins: [
-	// 	require('@tailwindcss/typography'),
-	// 	// require('daisyui')
-	// ],
+	theme: {
+		extend: {
+			fontFamily: {
+				heading: ['"DM Sans"', 'sans-serif'],
+				body: ['"Fira Sans"', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('daisyui')
+	],
 	// daisyui: {
 	// 	themes: ['winter', 'night']
 	// },
