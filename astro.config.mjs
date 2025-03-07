fedc00b7f678484a7b09e48b1ccf64dbccb49346
Imran Khan <m_imran_khan@me.com>
2025-03-07 19:45:22 +0100

    wip: tailwind 4 rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
