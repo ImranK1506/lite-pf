0518de1a3c67c791ac329fca14ec8740585a496f
Imran Khan <m_imran_khan@me.com>
2025-03-16 09:53:53 +0100

    wip: start vue example project

---
+++ src/projects/vue-app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Hello from Vue!</h2>
+    <p>This is a Vue component in your Astro project.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyVueComponent',
+};
+</script>
