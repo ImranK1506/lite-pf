4e89b78eac7fa35e739079203b25119a92e4fe34
Imran Khan <m_imran_khan@me.com>
2025-03-22 13:05:04 +0100

    wip: vue config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vue from '@astrojs/vue';
 
 export default defineConfig({
-  integrations: [tailwind(), react(),],
+  integrations: [
+    tailwind(),
+    react(),
+    vue()],
 });
